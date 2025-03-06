b364250332ba76210e1e1634acb92edbad6debb3
Richard Lander <rlander@microsoft.com>
2025-03-05 17:30:21 -0800

    Check if tag is null

--- src/Shaken/Program.cs
+++ src/Shaken/Program.cs
@@ -56,6 +56,13 @@ static async Task<string> Shaken(string line)
     Console.WriteLine($"ImageName: {imageName}");
     using RegistryClient client = new(imageName.Registry ?? registry);
 
+    // Tag is required for digest
+    if (imageName.Tag is null)
+    {
+        Console.WriteLine($"ImageName: {imageName}");
+        throw new Exception($"ImageName.Tag is null");
+    }
+
     // Goal format:
     // mcr.microsoft.com/dotnet-buildtools/prereqs:debian-12-helix-arm32v7@sha256:f765e1228b4977a6a18edd88702d444a7ffaa550c7c5b23097635fbdda41e81d
     string digest = await client.Manifests.GetDigestAsync(imageName.Repo, imageName.Tag);
